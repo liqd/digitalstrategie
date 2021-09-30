8819962c7e54e9097b70bd234a40ef810b4ebace
Julian Dehm <j.dehm@liqd.net>
2021-09-30 10:33:15 +0200

    add ci config
    * add empty test so the ci doesn't throw an error
    * fix requirements to include psycopg2

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-
 # digitalstrategie
+
+[![Coverage Status](https://coveralls.io/repos/github/liqd/digitalstrategie/badge.svg?branch=main)](https://coveralls.io/github/liqd/digitalstrategie?branch=main)
 
 Welcome to digitalstrategie
 
---
+++ digitalstrategie/settings/travis.py
@@ -0,0 +1,12 @@
+from .test import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'django',
+        'TEST': {
+            'NAME': 'django_test'
+        },
+    }
+}

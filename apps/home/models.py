34b71d89d65c5f8e08a45e7c608011642115df0a
rine <k.schmid@liqd.net>
2021-10-01 16:39:02 +0200

    contrib/home/images: make fields translateable

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,51 +1,100 @@
 from django.db import models
-from wagtail.admin import edit_handlers
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import ObjectList
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.admin.edit_handlers import TabbedInterface
 from wagtail.core import blocks, fields
 from wagtail.core.models import Page
-from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from apps import blocks as apps_blocks
+from apps.contrib.translations import TranslatedField
 
 
 class HomePage(Page):
-    body = fields.StreamField([
+
+    page_blocks = [
         ('paragraph', blocks.RichTextBlock()),
         ('call_to_action', apps_blocks.CallToActionBlock()),
         ('image_call_to_action', apps_blocks.ImageCallToActionBlock()),
         ('columns_text', apps_blocks.ColumnsBlock()),
         ('faq_accordion', apps_blocks.FaqBlock())
-    ])
+    ]
 
-    subtitle = models.CharField(max_length=120, blank=True)
+    subtitle_de = models.CharField(max_length=120, blank=True)
+    subtitle_en = models.CharField(max_length=120, blank=True)
+
+    body_de = fields.StreamField(page_blocks)
+    body_en = fields.StreamField(page_blocks, blank=True)
 
     header_image = models.ForeignKey(
-                      'wagtailimages.Image',
+                      'apps_images.CustomImage',
                       null=True,
                       blank=True,
                       on_delete=models.SET_NULL,
                       related_name='+'
                       )
 
-    content_panels = [
-        edit_handlers.FieldPanel('title'),
-        edit_handlers.FieldPanel('subtitle'),
-        ImageChooserPanel('header_image'),
-        edit_handlers.StreamFieldPanel('body')
+    subtitle = TranslatedField(
+        'subtitle_de',
+        'subtitle_en'
+    )
+
+    body = TranslatedField(
+        'body_de',
+        'body_en'
+    )
+
+    de_content_panels = [
+        FieldPanel('subtitle_de'),
+        StreamFieldPanel('body_de'),
     ]
+
+    en_content_panels = [
+        FieldPanel('subtitle_en'),
+        StreamFieldPanel('body_en'),
+    ]
+
+    common_panels = [
+        FieldPanel('title'),
+        ImageChooserPanel('header_image'),
+    ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(common_panels, heading='Common'),
+        ObjectList(de_content_panels, heading='German'),
+        ObjectList(en_content_panels, heading='English'),
+    ])
 
     subpage_types = ['apps_home.SimplePage']
 
 
 class SimplePage(Page):
-    body = fields.StreamField([
+
+    page_blocks = [
         ('paragraph', blocks.RichTextBlock()),
         ('faq_accordion', apps_blocks.FaqBlock())
-    ])
-
-    content_panels = [
-        edit_handlers.FieldPanel('title'),
-        edit_handlers.StreamFieldPanel('body'),
     ]
+
+    body_de = fields.StreamField(page_blocks)
+    body_en = fields.StreamField(page_blocks, blank=True)
+
+    body = TranslatedField(
+        'body_de',
+        'body_en'
+    )
+
+    de_content_panels = [
+        StreamFieldPanel('body_de'),
+    ]
+
+    en_content_panels = [
+        StreamFieldPanel('body_en'),
+    ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(de_content_panels, heading='German'),
+        ObjectList(en_content_panels, heading='English'),
+    ])
 
 subpage_types = []

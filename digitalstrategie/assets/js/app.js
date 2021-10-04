4797845a3d52e25f081dd22cdd5332297381fd00
phillimorland <5871230+phillimorland@users.noreply.github.com>
2021-10-04 08:13:37 +0200

    assets/app.js: only load required bootstrap components

--- digitalstrategie/assets/js/app.js
+++ digitalstrategie/assets/js/app.js
@@ -1,7 +1,8 @@
 /* eslint no-unused-vars: "off", no-new: "off" */
 /* global location */
-
-// load bootstrap components
-require('bootstrap')
-
-var django = require('django')
+import 'bootstrap/js/dist/button'
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/tab' // load bootstrap components
+import 'django'

164e3a5feec754652d21252bf07aa220d0066ec5
rine <k.schmid@liqd.net>
2021-10-04 12:49:14 +0200

    contrib/translations: add easy german as language option

--- apps/contrib/translations.py
+++ apps/contrib/translations.py
@@ -4,9 +4,10 @@ from wagtail.core.blocks.stream_block import StreamValue
 
 class TranslatedField(object):
 
-    def __init__(self, de_field, en_field):
+    def __init__(self, de_field, en_field, de_ls_field):
         self.de_field = de_field
         self.en_field = en_field
+        self.de_ls_field = de_ls_field
 
     def hasContent(self, field):
         if isinstance(field, StreamValue):
@@ -22,8 +23,11 @@ class TranslatedField(object):
     def __get__(self, instance, owner):
         de = getattr(instance, self.de_field)
         en = getattr(instance, self.en_field)
+        de_ls = getattr(instance, self.de_ls_field)
 
         if translation.get_language() == 'en' and self.hasContent(en):
             return en
+        elif translation.get_language() == 'de-ls' and self.hasContent(de_ls):
+            return de_ls
         else:
             return de

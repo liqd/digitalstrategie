922667017d6ef08ea928b23d64e75b96bd31bae0
rine <k.schmid@liqd.net>
2021-10-06 19:52:34 +0200

    contrib/templatetags: add form_field_tags

---
+++ apps/contrib/templatetags/form_field_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from django.forms import CheckboxInput
+from django.forms import RadioSelect
+
+register = template.Library()
+
+
+@register.filter(name='is_checkbox')
+def is_checkbox(field):
+    return (field.field.widget.__class__.__name__
+            == CheckboxInput().__class__.__name__)

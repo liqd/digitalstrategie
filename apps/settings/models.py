5c9f8d3289884dacd78b437e283192ca4b337276
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-10-08 12:33:43 +0200

    apps/gruenbuch: add settings for download and use in template

--- apps/settings/models.py
+++ apps/settings/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.admin.edit_handlers import PageChooserPanel
 from wagtail.contrib.settings.models import BaseSetting
 from wagtail.contrib.settings.models import register_setting
+from wagtail.documents.edit_handlers import DocumentChooserPanel
 
 
 @register_setting
@@ -24,4 +25,19 @@ class ImportantPages(BaseSetting):
                          ['apps_home.SimplePage']),
         PageChooserPanel('data_protection_policy',
                          ['apps_home.SimplePage']),
+    ]
+
+
+@register_setting
+class Downloads(BaseSetting):
+    gruenbuch = models.ForeignKey(
+        'wagtaildocs.Document',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='gruenbuch_pdf'
+    )
+
+    panels = [
+        DocumentChooserPanel('gruenbuch'),
     ]

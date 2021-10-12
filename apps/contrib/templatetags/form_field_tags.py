3cd2aa1e054d26beb882b1a9e6c9d135fe8df319
rine <k.schmid@liqd.net>
2021-10-12 14:33:16 +0200

    apps/contrib//forms: add templatetag to detect checkbox select multiple and use

--- apps/contrib/templatetags/form_field_tags.py
+++ apps/contrib/templatetags/form_field_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from django.forms import CheckboxInput
-from django.forms import RadioSelect
+from django.forms import CheckboxSelectMultiple
 
 register = template.Library()
 
@@ -9,3 +9,9 @@ register = template.Library()
 def is_checkbox(field):
     return (field.field.widget.__class__.__name__
             == CheckboxInput().__class__.__name__)
+
+
+@register.filter(name='is_checkbox_select_multiple')
+def is_checkbox_select_multiple(field):
+    return (field.field.widget.__class__.__name__
+            == CheckboxSelectMultiple().__class__.__name__)

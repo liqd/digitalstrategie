5a8bbb8929ff376e864336cc456aa01b54e70861
rine <k.schmid@liqd.net>
2021-10-18 11:36:38 +0200

    apps/home/blocks: add teaser blocks and change helptexts of background colour in all blocks

--- apps/home/blocks.py
+++ apps/home/blocks.py
@@ -18,7 +18,7 @@ class CallToActionBlock(blocks.StructBlock):
     link = blocks.PageChooserBlock(required=False)
     link_text = blocks.CharBlock(max_length=50, label='Link Text')
     background_color = ColorChoiceBlock(
-        help_text='Not choosing a colour will result in a CTA '
+        help_text='Not choosing a colour will result in a '
                   'block with a white background.',
         required=False
     )
@@ -44,7 +44,7 @@ class FaqBlock(blocks.StructBlock):
     )
     accordion_items = blocks.ListBlock(AccordionItemBlock())
     background_color = ColorChoiceBlock(
-        help_text=('Not choosing a colour will result in an FAQ block with '
+        help_text=('Not choosing a colour will result in a block with '
                    'a white background.'),
         required=False
     )
@@ -68,7 +68,7 @@ class QuoteBlock(blocks.StructBlock):
         required=False
     )
     background_color = ColorChoiceBlock(
-        help_text='Not choosing a colour will result in a Quote block with '
+        help_text='Not choosing a colour will result in a block with '
                   'a white background.',
         required=False
     )
@@ -95,7 +95,7 @@ class TextWithImageBlock(blocks.StructBlock):
     image = ImageChooserBlock(required=True)
 
     background_color = ColorChoiceBlock(
-        help_text='Not choosing a colour will result in a Quote block with '
+        help_text='Not choosing a colour will result in a block with '
                   'a white background.',
         required=False
     )
@@ -103,3 +103,94 @@ class TextWithImageBlock(blocks.StructBlock):
     class Meta:
         template = 'apps_home/blocks/text_with_image_block.html'
         icon = 'arrow-down'
+
+
+# teaser blocks
+
+class TeaserBlockCentered(blocks.StructBlock):
+
+    link = blocks.PageChooserBlock(
+        target_model=['apps_home.DetailPage',
+                      'apps_gruenbuch.GruenbuchIndexPage'],
+        help_text='Make sure that teaser title and teaser intro '
+                  'of the chosen page are set.'
+    )
+    link_text = blocks.CharBlock(max_length=50, label='Link Text')
+    background_color = ColorChoiceBlock(
+        help_text='Not choosing a colour will result in a '
+                  'block with a white background.',
+        required=False
+    )
+
+    class Meta:
+        template = 'apps_home/blocks/teaser_block_centered.html'
+        icon = 'arrow-down'
+
+
+class TeaserBlockTwoColumns(blocks.StructBlock):
+
+    link_1 = blocks.PageChooserBlock(
+        target_model=['apps_home.DetailPage',
+                      'apps_gruenbuch.GruenbuchIndexPage'],
+        help_text='Make sure that teaser title and teaser intro '
+                  'of the chosen page are set.'
+    )
+    link_1_text = blocks.CharBlock(max_length=50, label='Link Text')
+
+    link_2 = blocks.PageChooserBlock(
+        target_model=['apps_home.DetailPage',
+                      'apps_gruenbuch.GruenbuchIndexPage'],
+        help_text='Make sure that teaser title and teaser intro '
+                  'of the chosen page are set.'
+    )
+    link_2_text = blocks.CharBlock(max_length=50, label='Link Text')
+
+    background_color = ColorChoiceBlock(
+        help_text='Not choosing a colour will result in a '
+                  'block with a white background.',
+        required=False
+    )
+
+    class Meta:
+        template = 'apps_home/blocks/teaser_block_two_columns.html'
+        icon = 'arrow-down'
+
+
+class TeaserBlockImageLeft(blocks.StructBlock):
+
+    link = blocks.PageChooserBlock(
+        target_model=['apps_home.DetailPage',
+                      'apps_gruenbuch.GruenbuchIndexPage'],
+        help_text='Make sure that teaser title, teaser intro and teaser image '
+                  'of the chosen page are set.'
+    )
+    link_text = blocks.CharBlock(max_length=50, label='Link Text')
+    background_color = ColorChoiceBlock(
+        help_text='Not choosing a colour will result in a '
+                  'block with a white background.',
+        required=False
+    )
+
+    # class Meta:
+    #     template = 'apps_home/blocks/gruenbuch_faq_block.html'
+    #     icon = 'arrow-down'
+
+
+class TeaserBlockImageRight(blocks.StructBlock):
+
+    link = blocks.PageChooserBlock(
+        target_model=['apps_home.DetailPage',
+                      'apps_gruenbuch.GruenbuchIndexPage'],
+        help_text='Make sure that teaser title, teaser intro and teaser image '
+                  'of the chosen page are set.'
+    )
+    link_text = blocks.CharBlock(max_length=50, label='Link Text')
+    background_color = ColorChoiceBlock(
+        help_text='Not choosing a colour will result in a '
+                  'block with a white background.',
+        required=False
+    )
+
+    # class Meta:
+    #     template = 'apps_home/blocks/gruenbuch_faq_block.html'
+    #     icon = 'arrow-down'

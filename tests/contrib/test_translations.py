353c730813e6ea91b5f82bb3cae77cfafe12b084
Julian Dehm <j.dehm@liqd.net>
2021-11-24 18:01:58 +0100

    tests: add simple test for searchable fields

---
+++ tests/contrib/test_translations.py
@@ -0,0 +1,15 @@
+from django.utils import translation
+
+from apps.contrib.translations import get_search_fields
+
+
+def test_get_search_fields():
+    with translation.override("en"):
+        fields = get_search_fields()
+        assert all(map(lambda field: field.endswith("_en"), fields))
+    with translation.override("de"):
+        fields = get_search_fields()
+        assert all(map(lambda field: field.endswith("_de"), fields))
+    with translation.override("de_ls"):
+        fields = get_search_fields()
+        assert all(map(lambda field: field.endswith("_de-ls"), fields))

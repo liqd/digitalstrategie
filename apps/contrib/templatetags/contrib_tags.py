7b1665b9f59eb4247e1b0629886b974164770a3f
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-02-08 10:31:39 +0100

    settings/footer: move external footer url to settings

--- apps/contrib/templatetags/contrib_tags.py
+++ apps/contrib/templatetags/contrib_tags.py
@@ -1,5 +1,6 @@
 import requests
 from django import template
+from django.conf import settings
 from django.utils.html import mark_safe
 
 register = template.Library()
@@ -7,5 +8,8 @@ register = template.Library()
 
 @register.simple_tag
 def get_external_footer():
-    response = requests.get('https://www.berlin.de/rbmskzl/__i9/ohne/foot.inc')
-    return mark_safe(response.text)
+    if hasattr(settings, 'BERLIN_FOOTER_URL') and settings.BERLIN_FOOTER_URL:
+        response = requests.get(settings.BERLIN_FOOTER_URL)
+        return mark_safe(response.text)
+    else:
+        return ''

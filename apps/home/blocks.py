719fec16481a9e87b4542b64d8902276ed2c797b
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-02-10 13:13:50 +0100

    blocks.py//templates/blocks/teaser_*: adding additional language fields for teaser link text and adjusting templates partial fix #201

--- apps/home/blocks.py
+++ apps/home/blocks.py
@@ -121,87 +121,6 @@ class CoulouredParagraphBlock(blocks.StructBlock):
         icon = 'doc-full'
 
 
-class TeaserBlockCentered(blocks.StructBlock):
-
-    link = blocks.PageChooserBlock(
-        target_model=['apps_home.DetailPage',
-                      'apps_gruenbuch.GruenbuchIndexPage',
-                      'apps_forms.FormPage'],
-        help_text='Make sure that teaser title and teaser intro '
-                  'of the chosen page are set.'
-    )
-    link_text = blocks.CharBlock(max_length=50, label='Link Text')
-    background_color = ColorChoiceBlock(
-        help_text='Not choosing a colour will result in a '
-                  'block with a white background.',
-        required=False
-    )
-
-    class Meta:
-        template = 'apps_home/blocks/teaser_block_centered.html'
-        icon = 'arrow-down'
-
-
-class TeaserBlockTwoColumns(blocks.StructBlock):
-
-    link_1 = blocks.PageChooserBlock(
-        target_model=['apps_home.DetailPage',
-                      'apps_gruenbuch.GruenbuchIndexPage',
-                      'apps_forms.FormPage'],
-        help_text='Make sure that teaser title and teaser intro '
-                  'of the chosen page are set.'
-    )
-    link_1_text = blocks.CharBlock(max_length=50, label='Link Text')
-
-    link_2 = blocks.PageChooserBlock(
-        target_model=['apps_home.DetailPage',
-                      'apps_gruenbuch.GruenbuchIndexPage'],
-        help_text='Make sure that teaser title and teaser intro '
-                  'of the chosen page are set.'
-    )
-    link_2_text = blocks.CharBlock(max_length=50, label='Link Text')
-
-    background_color = ColorChoiceBlock(
-        help_text='Not choosing a colour will result in a '
-                  'block with a white background.',
-        required=False
-    )
-
-    class Meta:
-        template = 'apps_home/blocks/teaser_block_two_columns.html'
-        icon = 'arrow-down'
-
-
-class TeaserBlockImage(blocks.StructBlock):
-
-    link = blocks.PageChooserBlock(
-        target_model=['apps_home.DetailPage',
-                      'apps_gruenbuch.GruenbuchIndexPage',
-                      'apps_forms.FormPage'],
-        help_text='Make sure that teaser title, teaser intro and teaser image '
-                  'of the chosen page are set.'
-    )
-    link_text = blocks.CharBlock(max_length=50, label='Link Text')
-    background_color = ColorChoiceBlock(
-        help_text='Not choosing a colour will result in a '
-                  'block with a white background.',
-        required=False
-    )
-    image_alignment = blocks.ChoiceBlock(
-        choices=[
-            ('left', 'left'),
-            ('right', 'right'),
-        ],
-        icon='cup',
-        default='left',
-        help_text='How should the image be aligned?'
-    )
-
-    class Meta:
-        template = 'apps_home/blocks/teaser_block_image.html'
-        icon = 'image'
-
-
 class ThesisBlock(blocks.StructBlock):
     thesis = blocks.CharBlock(label='One thesis')
 
@@ -225,3 +144,138 @@ class ThesisListBlock(blocks.StructBlock):
     class Meta:
         template = 'apps_home/blocks/thesis_list_block.html'
         icon = 'list-ul'
+
+
+# Teaser blocks
+class TeaserBlockCentered(blocks.StructBlock):
+
+    link = blocks.PageChooserBlock(
+        target_model=['apps_home.DetailPage',
+                      'apps_gruenbuch.GruenbuchIndexPage',
+                      'apps_forms.FormPage'],
+        help_text='Make sure that teaser title and teaser intro '
+                  'of the chosen page are set.'
+    )
+    link_text_de = blocks.CharBlock(
+        max_length=50,
+        label='Link Text de'
+    )
+    link_text_en = blocks.CharBlock(
+        max_length=50,
+        label='Link Text en',
+        required=False
+    )
+    link_text_de_ls = blocks.CharBlock(
+        max_length=50,
+        label='Link Text de ls',
+        required=False
+    )
+
+    background_color = ColorChoiceBlock(
+        help_text='Not choosing a colour will result in a '
+                  'block with a white background.',
+        required=False
+    )
+
+    class Meta:
+        template = 'apps_home/blocks/teaser_block_centered.html'
+        icon = 'arrow-down'
+
+
+class TeaserBlockTwoColumns(blocks.StructBlock):
+
+    link_1 = blocks.PageChooserBlock(
+        target_model=['apps_home.DetailPage',
+                      'apps_gruenbuch.GruenbuchIndexPage',
+                      'apps_forms.FormPage'],
+        help_text='Make sure that teaser title and teaser intro '
+                  'of the chosen page are set.'
+    )
+    link_1_text_de = blocks.CharBlock(
+        max_length=50,
+        label='Link Text de'
+    )
+    link_1_text_en = blocks.CharBlock(
+        max_length=50,
+        label='Link Text en',
+        required=False
+    )
+    link_1_text_de_ls = blocks.CharBlock(
+        max_length=50,
+        label='Link Text de ls',
+        required=False
+    )
+
+    link_2 = blocks.PageChooserBlock(
+        target_model=['apps_home.DetailPage',
+                      'apps_gruenbuch.GruenbuchIndexPage'],
+        help_text='Make sure that teaser title and teaser intro '
+                  'of the chosen page are set.'
+    )
+    link_2_text_de = blocks.CharBlock(
+        max_length=50,
+        label='Link Text de'
+    )
+    link_2_text_en = blocks.CharBlock(
+        max_length=50,
+        label='Link Text en',
+        required=False
+    )
+    link_2_text_de_ls = blocks.CharBlock(
+        max_length=50,
+        label='Link Text de ls',
+        required=False
+    )
+
+    background_color = ColorChoiceBlock(
+        help_text='Not choosing a colour will result in a '
+                  'block with a white background.',
+        required=False
+    )
+
+    class Meta:
+        template = 'apps_home/blocks/teaser_block_two_columns.html'
+        icon = 'arrow-down'
+
+
+class TeaserBlockImage(blocks.StructBlock):
+
+    link = blocks.PageChooserBlock(
+        target_model=['apps_home.DetailPage',
+                      'apps_gruenbuch.GruenbuchIndexPage',
+                      'apps_forms.FormPage'],
+        help_text='Make sure that teaser title, teaser intro and teaser image '
+                  'of the chosen page are set.'
+    )
+    link_text_de = blocks.CharBlock(
+        max_length=50,
+        label='Link Text de'
+    )
+    link_text_en = blocks.CharBlock(
+        max_length=50,
+        label='Link Text en',
+        required=False
+    )
+    link_text_de_ls = blocks.CharBlock(
+        max_length=50,
+        label='Link Text de ls',
+        required=False
+    )
+    background_color = ColorChoiceBlock(
+        help_text='Not choosing a colour will result in a '
+                  'block with a white background.',
+        required=False
+    )
+    image_alignment = blocks.ChoiceBlock(
+        choices=[
+            ('left', 'left'),
+            ('right', 'right'),
+        ],
+        icon='cup',
+        default='left',
+        help_text='How should the image be aligned?'
+    )
+
+    class Meta:
+        template = 'apps_home/blocks/teaser_block_image.html'
+        icon = 'image'

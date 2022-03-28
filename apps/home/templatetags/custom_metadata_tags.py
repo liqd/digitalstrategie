ecad7eeec0bf9f07e85d8ef577e319f7dfccec9f
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-03-28 18:18:03 +0200

    apps/home: add custom metadata tags to use tags from homepage if page does not have own tags

---
+++ apps/home/templatetags/custom_metadata_tags.py
@@ -0,0 +1,24 @@
+from django import template
+from wagtail.core.models import Site
+from wagtailmetadata import tags
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def custom_meta_tags(context, model=None):
+    """Use metadata from homepage if not set on current page.
+
+    Like https://github.com/neon-jungle/wagtail-metadata/blob/master/
+    wagtailmetadata/templatetags/wagtailmetadata_tags.py
+    but uses homepage meta tags if current site does not have own tags.
+    """
+    request = context.get('request', None)
+    if not model:
+        model = context.get('self', None)
+        if not hasattr(model, 'get_meta_description') \
+                or not model.get_meta_description():
+            site = Site.find_for_request(request)
+            model = site.root_page.specific
+
+    return tags.meta_tags(request, model)

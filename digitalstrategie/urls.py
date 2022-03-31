b24787aac99b47d89a18b646a906fe4d7005f285
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-03-31 14:41:13 +0200

    urls: fix deprecation of django.conf.urls

--- digitalstrategie/urls.py
+++ digitalstrategie/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.conf.urls import include
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include
+from django.urls import path
+from django.urls import re_path
 from django.views.generic import TemplateView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.contrib.sitemaps.views import sitemap as wagtail_sitemap
@@ -11,20 +12,20 @@ from wagtail.documents import urls as wagtaildocs_urls
 from apps.contrib.views import SearchResultsView
 
 urlpatterns = [
-    url(r'^django-admin/', admin.site.urls),
-    url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
-    url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^sitemap\.xml$', wagtail_sitemap),
-    url(r'^robots\.txt$', TemplateView.as_view(
+    path('django-admin/', admin.site.urls),
+    path('admin/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('i18n/', include('django.conf.urls.i18n')),
+    re_path(r'^sitemap\.xml$', wagtail_sitemap),
+    re_path(r'^robots\.txt$', TemplateView.as_view(
         template_name='robots.txt',
         content_type="text/plain"), name="robots_file"),
-    url(r'^search/', SearchResultsView.as_view(), name="search"),
+    path('search/', SearchResultsView.as_view(), name="search"),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    url(r'', include(wagtail_urls)),
+    path('', include(wagtail_urls)),
 
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:

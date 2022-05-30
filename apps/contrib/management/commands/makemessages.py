93659da2c5f70c5dcd3c7a0000c4a241869f3785
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-05-30 19:14:16 +0200

    translations: overwrite makemessages to not allow fuzzy matching

---
+++ apps/contrib/management/commands/makemessages.py
@@ -0,0 +1,7 @@
+from django.core.management.commands import makemessages
+
+
+class Command(makemessages.Command):
+    msgmerge_options = (
+        makemessages.Command.msgmerge_options + ['--no-fuzzy-matching']
+    )

dcbeaf8cb30f2cbc8749676558df7add413ed73d
Julian Dehm <j.dehm@liqd.net>
2023-04-13 11:48:48 +0200

    improve footer handling
    * load footer from file instead of requesting it every time
    * load footer from cache if available

--- apps/contrib/templatetags/contrib_tags.py
+++ apps/contrib/templatetags/contrib_tags.py
@@ -1,6 +1,9 @@
-import requests
+import os
+
 from django import template
 from django.conf import settings
+from django.core import management
+from django.core.cache import cache
 from django.utils.html import mark_safe
 
 register = template.Library()
@@ -8,11 +11,20 @@ register = template.Library()
 
 @register.simple_tag
 def get_external_footer():
+    # The BO footer needs to be included here as we can't include external
+    # html in the browser. Add a cron job to update the file periodically via
+    # the get_footer management command.
     if hasattr(settings, 'BERLIN_FOOTER_URL') and settings.BERLIN_FOOTER_URL:
-        try:
-            response = requests.get(settings.BERLIN_FOOTER_URL, timeout=2.0)
-        except (requests.ConnectTimeout, requests.HTTPError, requests.Timeout):
-            return ''
-        return mark_safe(response.text)
-    else:
-        return ''
+        footer = cache.get('footer')
+        if footer:
+            return footer
+        filepath = settings.MEDIA_ROOT + "/landesfooter.inc"
+        if not os.path.isfile(filepath):
+            has_error = management.call_command('get_footer')
+            if has_error:
+                return ''
+        with open(filepath, "r") as f:
+            footer = mark_safe(f.read())
+            cache.set('footer', footer)
+            return footer
+    return ''

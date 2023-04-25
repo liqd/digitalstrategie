3d9095703d1e441e439470b00fce52eaa9eac85c
Julian Dehm <j.dehm@liqd.net>
2023-04-25 16:52:41 +0200

    add initial content-security-policy
    * use django-csp
    * add csp for dev to find issues

--- digitalstrategie/settings/dev.py
+++ digitalstrategie/settings/dev.py
@@ -32,14 +32,43 @@ INSTALLED_APPS += ('wagtail.contrib.styleguide',)
 
 if os.getenv("DATABASE") == "postgresql":
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '5557',
-        'OPTIONS': {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'django',
+            'USER': 'django',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '5557',
+            'OPTIONS': {
             },
         }
     }
+
+# CSP for development (not very strict)
+CSP_DEFAULT_SRC = ["'self'"]
+# unsafe-eval only for testing
+CSP_SCRIPT_SRC = ["'unsafe-eval'"]
+CSP_SCRIPT_SRC_ATTR = ["'none'"]
+# wagtail (and webpack during dev) requires unsafe-inline
+CSP_SCRIPT_SRC_ELEM = ["'self'", "berlin.de",
+                       "www.berlin.de", "'unsafe-inline'"]
+CSP_IMG_SRC = ["'self'", "berlin.de", "www.berlin.de", "www.gravatar.com"]
+CSP_OBJECT_SRC = ["'none'"]
+CSP_MEDIA_SRC = ["'self'"]
+CSP_FRAME_SRC = ["'self'", "youtube.com",
+                 "www.youtube.com", "vimeo.com", "www.vimeo.com"]
+CSP_FONT_SRC = ["'self'", "berlin.de", "www.berlin.de"]
+CSP_CONNECT_SRC = ["'self'", "releases.wagtail.io"]
+CSP_STYLE_SRC = ["'none'"]
+# wagtail userbar requires unsafe-inline for wagtail <= 4.1
+CSP_STYLE_SRC_ATTR = ["'self'", "'unsafe-inline'"]
+# wagtail admin vendor.js requires unsafe-inline
+CSP_STYLE_SRC_ELEM = ["'self'", "berlin.de",
+                      "www.berlin.de", "'unsafe-inline'"]
+CSP_BASE_URI = ["'self'"]
+CSP_CHILD_SRC = ["'none'"]
+CSP_FRAME_ANCESTORS = ["'self'"]
+CSP_FORM_ACTION = ["'self'"]
+CSP_MANIFEST_SRC = ["'self'"]
+CSP_WORKER_SRC = ["'none'"]
+# CSP_UPGRADE_INSECURE_REQUESTS = True

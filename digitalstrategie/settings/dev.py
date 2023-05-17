5c9c83a92bb396bf1b381acedd086d9aa46ed233
Julian Dehm <j.dehm@liqd.net>
2023-05-17 14:40:44 +0200

    use stage captcha, fix csp for captcha

--- digitalstrategie/settings/dev.py
+++ digitalstrategie/settings/dev.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'b*1ljsb!x7@d_o$sohx-&q-7n*#r=lwhy542zxk(e=fj%ey3xp'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-CAPTCHA_URL = 'https://captcheck.netsyms.com/api.php'
+CAPTCHA_URL = 'https://captcha-numbers-stage.liqd.net/api.php'
 
 try:
     from .local import *
@@ -52,13 +52,13 @@ CSP_SCRIPT_SRC_ATTR = ["'none'"]
 # wagtail (and webpack during dev) requires unsafe-inline
 CSP_SCRIPT_SRC_ELEM = ["'self'", "berlin.de",
                        "www.berlin.de", "'unsafe-inline'"]
-CSP_IMG_SRC = ["'self'", "berlin.de", "www.berlin.de", "www.gravatar.com"]
+CSP_IMG_SRC = ["'self'", "captcha-numbers-stage.liqd.net", "berlin.de", "www.berlin.de", "www.gravatar.com"]
 CSP_OBJECT_SRC = ["'none'"]
 CSP_MEDIA_SRC = ["'self'"]
 CSP_FRAME_SRC = ["'self'", "youtube.com",
                  "www.youtube.com", "vimeo.com", "www.vimeo.com"]
 CSP_FONT_SRC = ["'self'", "berlin.de", "www.berlin.de"]
-CSP_CONNECT_SRC = ["'self'", "releases.wagtail.io"]
+CSP_CONNECT_SRC = ["'self'", "captcha-numbers-stage.liqd.net" "releases.wagtail.io"]
 CSP_STYLE_SRC = ["'none'"]
 # wagtail userbar requires unsafe-inline for wagtail <= 4.1
 CSP_STYLE_SRC_ATTR = ["'self'", "'unsafe-inline'"]

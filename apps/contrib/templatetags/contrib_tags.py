cd03c53937742605bc12c25c5fd7a6ef04f9bdd0
rine <k.schmid@liqd.net>
2023-05-31 16:39:47 +0200

    header_language_switch: add template tag to use translated url as link instead of POST to switch language, fixes #619

--- apps/contrib/templatetags/contrib_tags.py
+++ apps/contrib/templatetags/contrib_tags.py
@@ -6,6 +6,10 @@ from django.core import management
 from django.core.cache import cache
 from django.core.exceptions import ImproperlyConfigured
 from django.core.paginator import Paginator
+from django.http import Http404
+from django.urls import resolve
+from django.urls import reverse
+from django.utils import translation
 from django.utils.html import mark_safe
 
 register = template.Library()
@@ -101,3 +105,27 @@ def matomo_tracking_code():
         "url": settings.MATOMO_URL,
         "cookie_disabled": cookie_disabled,
     }
+
+
+@register.simple_tag(takes_context=True)
+def translate_url(context, lang=None, *args, **kwargs):
+    """Get active page's url by a specified language.
+
+    Usage: {% translate_url 'en' %}
+    """
+    path = context['request'].path
+    cur_language = translation.get_language()
+
+    try:
+        view = resolve(path)
+        translation.activate(lang)
+        url = reverse(
+            view.view_name,
+            args=view.args,
+            kwargs=view.kwargs,
+        )
+    except Http404:
+        url = '/' + lang + '/'
+    finally:
+        translation.activate(cur_language)
+    return url

f62d6ca720b67bb300ba91015b178ac0fcd89316
philli-m <5871230+philli-m@users.noreply.github.com>
2023-09-13 17:35:58 +0200

    home/blocks: add richtext to image-text block and change image with in richtect alligned image incl migrations

--- apps/home/blocks.py
+++ apps/home/blocks.py
@@ -64,7 +64,9 @@ class QuoteBlock(blocks.StructBlock):
 
 class TextWithImageBlock(blocks.StructBlock):
     title = blocks.CharBlock(required=True)
-    body = blocks.TextBlock(required=True, max_length=800)
+    body = blocks.RichTextBlock(features=['bold', 'italic', 'ol', 'ul',
+                                          'link'],
+                                required=True)
     image = ImageChooserBlock(required=True)
 
     image_alignment = blocks.ChoiceBlock(
